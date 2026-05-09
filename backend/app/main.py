f5684fff1d267e9327adb4e9fa207d87b31aa926
sooobin34 <ansubinjjang1@gmail.com>
2026-05-09 13:22:22 +0900

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://capstone-project-theta-amber.vercel.app",
+        "https://jeonbuk-mrv.vercel.app",
     
     ],
     allow_credentials=True,
