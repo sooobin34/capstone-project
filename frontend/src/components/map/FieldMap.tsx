c837142c67017886fade0b39796449dec5017d7f
kangsei <seii0020@naver.com>
2026-05-15 17:47:40 +0900

    feat: map 변경

--- frontend/src/components/map/FieldMap.tsx
+++ frontend/src/components/map/FieldMap.tsx
@@ -89,7 +89,7 @@ export default function FieldMap({ sensors, center, onNodeClick, selectedNodeId 
             : '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'}
           url={isSatellite
             ? 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-            : 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'}
+            : 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'}
         />
         <MapUpdater center={center} />
         {sensors.map((sensor) => (
